21aad17ab301def5250ef15a8fda28faffc3396d
Kavya-Bhat <68507147+Kavya-Bhat@users.noreply.github.com>
2020-07-20 18:40:24 +0530

    fourth commit

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -14,22 +14,26 @@ public class Main
         ArrayList<Integer> p2Positions = new ArrayList<>();
 
         Game g = new Game();
+        RulesOfTheGame obj = new RulesOfTheGame();
+        obj.printGameRules();
+
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter player1's name:");
-        String p1 = scanner.nextLine();
-        g.setPlayer1(p1);
+        String player1 = scanner.nextLine();
+        g.setPlayer1(player1);
 
         System.out.println("Enter player2's name:");
-        String p2 = scanner.nextLine();
-        g.setPlayer1(p2);
+        String player2 = scanner.nextLine();
+        g.setPlayer1(player2);
+
 
         clrscr();
         int p1Wins = 0;
         int p2Wins = 0;
         int flag = 0;
 
-        for(int x = 1;flag == 0;x++)
+        for(int x = 1; flag == 0; x++)
         {
             clrscr();
             p1Positions.clear();
@@ -43,52 +47,53 @@ public class Main
                 int result = 0;
                 for (int i = 1; result == 0; i++)
                 {
-                    System.out.println(p1 + " - X");
-                    System.out.println(p2 + " - O");
+                    System.out.println(player1 + " - X");
+                    System.out.println(player2 + " - O");
                     System.out.println();
                     System.out.println();
 
                     if (i % 2 == 1)
                     {
-                        System.out.println("It is " + p1 + "'s turn. " + p1 + ", please enter a number between 1 to 9 to position your symbol.");
+                        System.out.println("It is " + player1 + "'s turn. " + player1 + ", please enter a number between 1 to 9 to position your symbol.");
                         System.out.println();
                         g.printBoard();
-                        int p1Pos = scanner.nextInt();
-                        while (p1Positions.contains(p1Pos) || p2Positions.contains(p1Pos) || p1Pos < 0 || p1Pos > 9)
+                        int p1Position = scanner.nextInt();
+                        while (p1Positions.contains(p1Position) || p2Positions.contains(p1Position) || p1Position < 0 || p1Position > 9)
                         {
-                            if (p1Pos < 0 || p1Pos > 9)
+                            if (p1Position < 0 || p1Position > 9)
                             {
                                 System.out.println("Invalid number. Please enter a new number between 1 and 9");
-                                p1Pos = scanner.nextInt();
-                            } else
+                                p1Position = scanner.nextInt();
+                            }
+                            else
                             {
                                 System.out.println("Position already taken. Enter a number such that the position which you enter is empty.");
-                                p1Pos = scanner.nextInt();
+                                p1Position = scanner.nextInt();
                             }
                         }
-                        p1Positions.add(p1Pos);
-                        g.placePiece(p1Pos, 'X');
+                        p1Positions.add(p1Position);
+                        g.placePiece(p1Position, 'X');
                     }
                     else
                     {
-                        System.out.println("It is " + p2 + "'s turn. " + p2 + ", please enter a number between 1 to 9 to position your symbol.");
+                        System.out.println("It is " + player2 + "'s turn. " + player2 + ", please enter a number between 1 to 9 to position your symbol.");
                         System.out.println();
                         g.printBoard();
-                        int p2Pos = scanner.nextInt();
-                        while (p1Positions.contains(p2Pos) || p2Positions.contains(p2Pos) || p2Pos < 0 || p2Pos > 9)
+                        int p2Position = scanner.nextInt();
+                        while (p2Positions.contains(p2Position) || p2Positions.contains(p2Position) || p2Position < 0 || p2Position > 9)
                         {
-                            if (p2Pos < 0 || p2Pos > 9)
+                            if (p2Position < 0 || p2Position > 9)
                             {
                                 System.out.println("Invalid number. Please enter a new number between 1 and 9");
-                                p2Pos = scanner.nextInt();
+                                p2Position = scanner.nextInt();
                             } else
                             {
                                 System.out.println("Position already taken. Enter a number such that the position which you enter is empty.");
-                                p2Pos = scanner.nextInt();
+                                p2Position = scanner.nextInt();
                             }
                         }
-                        p2Positions.add(p2Pos);
-                        g.placePiece(p2Pos, 'O');
+                        p2Positions.add(p2Position);
+                        g.placePiece(p2Position, 'O');
                     }
 
                     clrscr();
@@ -97,17 +102,17 @@ public class Main
 
                 if(result == 10)
                 {
-                    System.out.println("Congrats " + p1 + ". You win!");
+                    System.out.println("Congrats " + player1 + ". You win!");
                     p1Wins++;
                 }
                 else if(result == 20)
                 {
-                    System.out.println("Congrats " + p2 + ". You win!");
+                    System.out.println("Congrats " + player2 + ". You win!");
                     p2Wins++;
                 }
                 else
                 {
-                    System.out.println("It's a tie between " + p1 + " and " + p2);
+                    System.out.println("It's a tie between " + player1 + " and " + player2);
                 }
                 System.out.println();
                 System.out.println("If you want to proceed to the next round, press 0. Else if you wanna start a new game, press 1. If you want to end the game, press 2");
@@ -121,7 +126,7 @@ public class Main
                 else if(flag == 2)
                 {
                     System.out.println("The Final score:");
-                    System.out.println(p1 + " - " + p1Wins + "\n" + p2 + " - " + p2Wins);
+                    System.out.println(player1 + " - " + p1Wins + "\n" + player2 + " - " + p2Wins);
                     System.out.println();
                     System.out.println("Thank you for playing this game! :)");
                     System.exit(0);
@@ -139,14 +144,14 @@ public class Main
                 int result = 0;
                 for (int i = 1; result == 0; i++)
                 {
-                    System.out.println(p1 + " - O");
-                    System.out.println(p2 + " - X");
+                    System.out.println(player1 + " - O");
+                    System.out.println(player2 + " - X");
                     System.out.println();
                     System.out.println();
 
                     if (i % 2 == 0)
                     {
-                        System.out.println("It is " + p1 + "'s turn. " + p1 + ", please enter a number between 1 to 9 to position your symbol.");
+                        System.out.println("It is " + player1 + "'s turn. " + player1 + ", please enter a number between 1 to 9 to position your symbol.");
                         System.out.println();
                         g.printBoard();
                         int p1Pos = scanner.nextInt();
@@ -167,7 +172,7 @@ public class Main
                     }
                     else
                     {
-                        System.out.println("It is " + p2 + "'s turn. " + p2 + ", please enter a number between 1 to 9 to position your symbol.");
+                        System.out.println("It is " + player2 + "'s turn. " + player2 + ", please enter a number between 1 to 9 to position your symbol.");
                         System.out.println();
                         g.printBoard();
                         int p2Pos = scanner.nextInt();
@@ -194,21 +199,21 @@ public class Main
                 if(result == 10)
                 {
                     g.printBoard();
-                    System.out.println("Congrats " + p1 + ". You win!");
+                    System.out.println("Congrats " + player1 + ". You win!");
                     p1Wins++;
                 }
                 else if(result == 20)
                 {
                     g.printBoard();
-                    System.out.println("Congrats " + p2 + ". You win!");
+                    System.out.println("Congrats " + player2 + ". You win!");
                     p2Wins++;
                 }
                 else
                 {
                     g.printBoard();
-                    System.out.println("It's a tie between " + p1 + " and " + p2);
+                    System.out.println("It's a tie between " + player1 + " and " + player2);
                 }
-                System.out.println("The score between" + p1 + " and " + p2 + " is:\n" + p1 + " - " + p1Wins + "\n" + p2 + " - " + p2Wins);
+                System.out.println("The score between" + player1 + " and " + player2 + " is:\n" + player1 + " - " + p1Wins + "\n" + player2 + " - " + p2Wins);
                 System.out.println();
                 System.out.println("If you want to proceed to the next round, press 0. Else if you wanna start a new game, press 1. If you want to end the game, press 2");
                 System.out.println("DO NOT Enter any other number other than 0 or 1 or 2. If you do so, the game will end");
@@ -221,7 +226,7 @@ public class Main
                 else if(flag == 2)
                 {
                     System.out.println("The Final score:");
-                    System.out.println(p1 + " - " + p1Wins + "\n" + p2 + " - " + p2Wins);
+                    System.out.println(player1 + " - " + p1Wins + "\n" + player2 + " - " + p2Wins);
                     System.out.println();
                     System.out.println("Thank you for playing this game! :)");
                     System.exit(0);
